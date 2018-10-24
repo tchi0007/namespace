0aab56556fde996432c51d1f0512bf3b6ee8c25f
tchi0007 <tchi0007@algonquinlive.com>
2018-10-24 18:24:02 -0400

    first upload

---
+++ namespace.js
@@ -0,0 +1,20 @@
+var TCHI0007 = {
+    init: function () {
+        let box = document.createElement("div");
+        box.className = "box";
+        box.textContent = "tchi0007";
+        let boxes = document.getElementById("boxes");
+        boxes.appendChild(box);
+
+        box.addEventListener("click", function () {
+            this.style.borderColor = "pink";
+            this.style.backgroundColor = "yellow";
+        });
+        box.addEventListener("mouseover", function () {
+            this.classList.toggle("highlight");
+        });
+        box.addEventListener("mouseout", function () {
+            this.classList.toggle("highlight");
+        });
+    }
+}
